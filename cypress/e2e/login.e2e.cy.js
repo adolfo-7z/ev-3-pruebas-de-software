b97957f94f1c6c0a7939f4d539f7862583a84063
adolfo-7z <a.plaza01@ufromail.cl>
2023-06-19 21:23:12 -0400

    login tests added

--- cypress/e2e/login.e2e.cy.js
+++ cypress/e2e/login.e2e.cy.js
@@ -1,5 +1,5 @@
 describe("Login E2E", () => {
-	it("Login incorrect", () => {
+	it("L-1 Login incorrect", () => {
 		cy.visit("/login");
 		cy.get('input[id="login-email"]').type("john@example.com");
 		cy.get('input[id="login-password"]').type("john@example.com");
@@ -11,7 +11,16 @@ describe("Login E2E", () => {
 			.and("contain.text", "invalid credentials");
 	});
 
-	it("Correct login", () => {
+	it("L-2 Login incorrect by wrong email", () => {
+		cy.visit("/login");
+		cy.get('input[id="login-email').type("juanito@example.com");
+		cy.get('input[id="login-password"]').type("2aSsword95%");
+		cy.get('button[id="login-submit"]').click();
+
+		cy.get("div").find(".q-notification").should("be.visible").and("contain.text", "invalid credentials");
+	});
+
+	it("L-4 Correct login", () => {
 		cy.visit("/login");
 		cy.get('input[type="email"]').type("john@example.com");
 		cy.get('input[type="password"]').type("2aSsword95%");
@@ -21,4 +30,15 @@ describe("Login E2E", () => {
 			cy.url().should("eq", `${Cypress.config().baseUrl}/`);
 		});
 	});
+
+	it("L-3 Login correct with unverified user", () => {
+		cy.visit("/login");
+		cy.get('input[id="login-email').type("che.copete12345@gmail.com");
+		cy.get('input[id="login-password"]').type("CheCopete123%");
+		cy.get('button[id="login-submit"]').click();
+
+		cy.url().should("eq", `${Cypress.config().baseUrl}/verify?verify_error=1`);
+		cy.get("div").find(".q-notification").should("be.visible").and("contain.text", "You need to verify your account");
+	});
+
 });
